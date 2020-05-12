23090b71b856357730dcbe080d43a0460c1ed1ed
savita <savita1310k@gmail.com>
2020-05-12 13:13:37 +0530

    designed order complete logo

--- src/components/OrderComplete.js
+++ src/components/OrderComplete.js
@@ -6,11 +6,28 @@ const OrderComplete = () => {
         <div className = "order-successfull">
             <div className = "order-complete">
                 <div>Order complete!</div>
-                <div style = {{color: "#0a9d5b"}}>This order has been fully dispatched.</div>
+                <div style = {{color: "#59B37F"}}>This order has been fully dispatched.</div>
             </div>
-            <div className = "order-complete-image">
-                <img src = {image} className = "image" alt = "order complete "/>
-            </div>
+            <div className="outer-circle">
+                <span className = "order-O">O</span>
+                <span className = "order-R">R</span>
+                <span className = "order-D">D</span>
+                <span className = "order-E">E</span>
+                <span className = "order-RR">R</span>
+
+                <span className = "C">C</span>
+                <span className = "O">O</span>
+                <span className = "M">M</span>
+                <span className = "P">P</span>
+                <span className = "L">L</span>
+                <span className = "E">E</span>
+                <span className = "T">T</span>
+                <span className = "EE">E</span>
+
+                <div className="inner-circle">
+                    <div className = "check"></div>
+                </div>
+                </div>
         </div>
     )
 }
